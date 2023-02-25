9abf77f880f18b4f661c9daf47a0abaa28563feb
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2023-02-25 10:10:15 +0300

    FIXED: clicking on empty space in itemList makes text color change

--- 01_base-app/src/components/employees-list-item/employees-list-item.jsx
+++ 01_base-app/src/components/employees-list-item/employees-list-item.jsx
@@ -33,23 +33,25 @@ class EmployeesListItem extends Component {
         <span className="list-group-item-label" onClick={() => this.onRise()}>
           {this.props.name}
         </span>
-        <input
-          type="text"
-          className="list-group-item-input"
-          defaultValue={this.props.salary + "$"}
-        />
-        <div className="d-flex justify-content-center align-items-center">
-          <button
-            type="button"
-            className="btn-cookie btn-sm "
-            onClick={() => this.onBonus()}
-          >
-            <i className="fas fa-cookie"></i>
-          </button>
-          <button type="button" className="btn-trash btn-sm ">
-            <i className="fas fa-trash"></i>
-          </button>
-          <i className="fas fa-star"></i>
+        <div className="d-flex">
+          <input
+            type="text"
+            className="list-group-item-input"
+            defaultValue={this.props.salary + "$"}
+          />
+          <div className="d-flex justify-content-center align-items-center">
+            <button
+              type="button"
+              className="btn-cookie btn-sm "
+              onClick={() => this.onBonus()}
+            >
+              <i className="fas fa-cookie"></i>
+            </button>
+            <button type="button" className="btn-trash btn-sm ">
+              <i className="fas fa-trash"></i>
+            </button>
+            <i className="fas fa-star"></i>
+          </div>
         </div>
       </li>
     );
