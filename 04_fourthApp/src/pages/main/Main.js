11c6859dd6c9f9796375cb3d366e64c6f9342d9b
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2023-04-22 12:46:46 +0300

    ADD: finished homework

--- 04_fourthApp/src/pages/main/Main.js
+++ 04_fourthApp/src/pages/main/Main.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { SingleCounter } from "../../components/singleCounter/SingleCounter";
+import { DoubleCounter } from "../../components/doubleCounter/DoubleCounter";
+import { GlobalCounter } from "../../components/globalCounter/GlobalCounter";
+import styles from "./Main.module.scss";
 
-export const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+export const Main = () => (
+  <div className={styles.root}>
+    <SingleCounter />
+    <DoubleCounter />
+    <GlobalCounter />
+  </div>
+);
