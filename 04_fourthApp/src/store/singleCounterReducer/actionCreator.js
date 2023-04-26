702a936f857643f99a53fa9015ce13fcb8c388df
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2023-04-26 23:49:44 +0300

    ADD: Small changes

--- 04_fourthApp/src/store/singleCounterReducer/actionCreator.js
+++ 04_fourthApp/src/store/singleCounterReducer/actionCreator.js
@@ -1,2 +1,4 @@
-export const increment = () => ({ type: "increment" });
-export const decrement = () => ({ type: "decrement" });
+import { ACTION_TYPES } from "../actionTypes";
+
+export const increment = () => ({ type: ACTION_TYPES.INCREMENT });
+export const decrement = () => ({ type: ACTION_TYPES.DECREMENT });
